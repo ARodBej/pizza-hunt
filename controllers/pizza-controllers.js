7fd8e23c0f38432fa5bbb60ff5d19152cad0b28f
Adilene Bejines <adi3511@hotmail.com>
2022-03-11 23:34:32 -0700

    fixed small issues

--- controllers/pizza-controllers.js
+++ controllers/pizza-controllers.js
@@ -11,22 +11,15 @@ const pizzaController = {
         });
     },
   
-    // get one pizza by id
-    getPizzaById({ params }, res) {
-      Pizza.findOne({ _id: params.id })
-        .then(dbPizzaData => {
-          // If no pizza is found, send 404
-          if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
-          }
-          res.json(dbPizzaData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
+  // get one pizza by id
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
 
     // createPizza
     createPizza({ body }, res) {
@@ -58,7 +51,7 @@ const pizzaController = {
       res.json(dbPizzaData);
     })
     .catch(err => res.status(400).json(err));
-}
   }
+};
 
 module.exports = pizzaController;
